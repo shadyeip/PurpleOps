ba2371c6538524e523d936d87086c62a327a4897
Adam Nadrowski <16740727+shadyeip@users.noreply.github.com>
2024-09-30 14:08:00 -0400

    Update generate_or_use_cert.sh

--- nginx/generate_or_use_cert.sh
+++ nginx/generate_or_use_cert.sh
@@ -4,7 +4,7 @@ if [ ! -f "/etc/nginx/ssl/nginx.key" ] || [ ! -f "/etc/nginx/ssl/nginx.crt" ]; t
     echo "Generating self-signed certificates"
     mkdir -p /etc/nginx/ssl
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-        -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt \
+        -keyout /etc/nginx/ssl/privkey.pem -out /etc/nginx/ssl/cert.pem \
         -subj "/C=US/ST=State/L=City/O=Organization/CN=purpleops"
 else
     echo "Nginx SSL certificates already exist"
